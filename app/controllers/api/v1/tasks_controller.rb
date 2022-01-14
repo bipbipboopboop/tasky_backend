731140fe1058b68f48a26dd8cabcb7f4591cba15
Your Name <ethanchen2009@hotmail.com>
2022-01-14 12:40:47 +0800

    Added Devise and JWT

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TasksController < ApplicationController
   before_action :set_task, only: [:show, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /tasks
   def index
     @tasks = Task.all
